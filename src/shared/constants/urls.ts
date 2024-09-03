f911b201b33bf0e3fb62cd7f733ce2e3d420d712
Gabriel-pagnan <gabrielviniciuspagnan@gmail.com>
2024-09-03 18:53:21 -0300

    build force

--- src/shared/constants/urls.ts
+++ src/shared/constants/urls.ts
@@ -2,4 +2,4 @@ export const URL_DEPARTMENT = process.env.REACT_APP_API_URL + '/departments';
 export const URL_DEPARTMENT_ID = process.env.REACT_APP_API_URL + '/departments/1';
 export const URL_USER = process.env.REACT_APP_API_URL + '/users/1';
 
-export const URL_AUTH = process.env.REACT_APP_API_URL + '/auth';
+export const URL_AUTH = process.env.REACT_APP_API_URL + '/auth'; 
